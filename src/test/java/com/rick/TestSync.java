22fc6c6454e37f5c0b2b986b70c250ccb4d652f8
Ricardo Osorio <ricardo8504@gmail.com>
2017-09-21 07:47:06 -0500

    se adicionan test async

--- src/test/java/com/rick/TestSync.java
+++ src/test/java/com/rick/TestSync.java
@@ -1,7 +1,6 @@
 package com.rick;
 
-import rx.Observable;
-import rx.schedulers.Schedulers;
+
 
 import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.*;
@@ -10,6 +9,15 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+
 
 public class TestSync {
 
@@ -20,7 +28,7 @@ public class TestSync {
 		Observable.create(s ->{
 			s.onNext("Hola Mundo Create 1");
 			s.onNext("Hola Mundo Create 2");
-			s.onCompleted();
+			
 		})
 				.subscribe(s -> {
 					assertThat(Arrays.asList(new String[] {"Hola Mundo Create 1","Hola Mundo Create 2"}),hasItem(s));
@@ -42,14 +50,51 @@ public class TestSync {
 		assertTrue(true);
 	}
 	
+	@Test
+	public void flowableAsyncBasic() {
+         Flowable.create((FlowableEmitter<Void> s) -> {
+                try {
+                		System.out.println("Se inicia el async");
+                        Thread.sleep(3000);
+                        System.out.println("Se finaliza el async");
+                } catch (Exception e) {
+                	e.printStackTrace();
+                }
+                s.onComplete();
+        }, BackpressureStrategy.BUFFER).subscribeOn(Schedulers.newThread()).subscribe();
+
+        System.out.println("Se finaliza el flowableAsyncBasic");
+	}	
 	
 	@Test
-	public void testJustSchedulers() {
-		Iterable<String> value = Observable.just("Hola Mundo Just Schedulers")
-			.subscribeOn(Schedulers.computation())
-			.toBlocking()
-			.toIterable();
-		assertEquals("Hola Mundo Just Schedulers",value.iterator().next());
+	public void simpleAsyncAPICalls() throws InterruptedException {
+        
+        Flowable.create((FlowableEmitter<String> s) -> {
+                try {
+                String result = makeCallString("http://sisglocotizacionesapi.herokuapp.com/sisglo/sinapsis/ordenes/status");
+                s.onNext(result);
+                } catch (Exception e) {
+                        s.onError(e);
+                }
+                s.onComplete();
+        }, BackpressureStrategy.BUFFER).subscribeOn(Schedulers.newThread()).subscribe(System.out::println);
+
+       Thread.currentThread().sleep(10000);
+     
+	}
+
+	private String makeCallString(String URI) {
+	        RestTemplate restTemplate = new RestTemplate();
+	        String result = restTemplate.getForObject(URI, String.class);
+	        return result;
+	}
+	
+	
+		
+	
+	public static void main(String args[]) throws InterruptedException {
+		new TestSync().flowableAsyncBasic();
+		Thread.currentThread().sleep(10000);
 	}
 	
 }
